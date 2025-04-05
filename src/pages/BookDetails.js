cffafe6972f156676482bcb609026cfbf67d3d75
rakhikhinder <rakhikhinder@yahoo.com>
2025-04-05 19:08:45 +0530

    fixes for api endpoints

--- src/pages/BookDetails.js
+++ src/pages/BookDetails.js
@@ -86,7 +86,7 @@ const BookDetails = () => {
     };
 
     try {
-      const res = await fetch("http://127.0.0.1:4000/api/reviews", {
+      const res = await fetch(`${API_BASE_URL}api/reviews`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(payload),
