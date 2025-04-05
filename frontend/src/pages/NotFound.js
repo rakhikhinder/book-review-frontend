e63011811ec366c3aa1cbcd9664dd67d8549b3f8
rakhikhinder@yahoo.com <rakhikhinder@yahoo.com>
2025-04-05 10:14:07 +0530

    complete the frontend

---
+++ frontend/src/pages/NotFound.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function NotFound() {
+  return (
+    <div className="text-center p-5">
+      <h1>404</h1>
+      <p>Page not found</p>
+    </div>
+  );
+}
+
+export default NotFound;
